8c12bb20004dfdc117afd1082ecdbe584139a25a
wbhart <goodwillhart@googlemail.com>
2009-03-28 04:32:45 +0000

    Fixed some bugs in toom4 and added sumdiffs.

--- mpn/generic/mul_tc4.c
+++ mpn/generic/mul_tc4.c
@@ -96,6 +96,265 @@ void tc4_add(mp_ptr rp, mp_size_t * rn, mp_ptr r1, mp_size_t r1n, mp_ptr r2, mp_
    }
 }
 
+void tc4_sumdiff(mp_ptr rp, mp_size_t * rn, mp_ptr sp, mp_size_t * sn, mp_ptr r1, mp_size_t r1n, mp_ptr r2, mp_size_t r2n)
+{
+   mp_limb_t cy, cy2;
+   mp_size_t s1 = ABS(r1n);
+   mp_size_t s2 = ABS(r2n);
+	int swapped = 0;
+   
+   if (s1 < s2) 
+   {
+      MPN_PTR_SWAP(r1, r1n, r2, r2n);
+      MP_SIZE_T_SWAP(s1, s2);
+		swapped = 1;
+   } 
+   
+   if (!s1)
+   {
+      *rn = 0;
+		*sn = 0;
+   } else if (!s2)
+   {
+      if (rp != r1) MPN_COPY(rp, r1, s1);
+		if (sp != r1) MPN_COPY(sp, r1, s1);
+		*rn = r1n;
+		*sn = (swapped ? -r1n : r1n);
+   } else
+   {
+      mp_size_t ct;
+		if (s1 != s2) ct = 1;
+		else MPN_CMP(ct, r1, r2, s1); 
+		    
+      if (!ct) 
+		{
+			if ((r1n ^ r2n) >= 0)
+			{
+				*sn = 0;
+				*rn = r1n;
+            cy = mpn_lshift(rp, r1, s1, 1);
+            if (cy) 
+            {
+               rp[s1] = cy;
+               if ((*rn) < 0) (*rn)--;
+               else (*rn)++;
+				}
+         } else
+			{
+				*rn = 0;
+				*sn = (swapped ? -r1n : r1n);
+            cy = mpn_lshift(sp, r1, s1, 1);
+            if (cy) 
+            {
+               sp[s1] = cy;
+               if ((*sn) < 0) (*sn)--;
+               else (*sn)++;
+				}
+			}
+		} else if (ct > 0) // r1 is bigger than r2
+      {
+         if ((r1n ^ r2n) >= 0) // both inputs are same sign
+			{
+				cy = mpn_sumdiff_n(rp, sp, r1, r2, s2);
+				if (s1 > s2) // r1 has more limbs than r2
+				{
+               *rn = r1n;
+               cy2 = mpn_add_1(rp + s2, r1 + s2, s1 - s2, cy>>1);
+					if (cy2) 
+               {
+                  rp[s1] = cy2;
+                  if ((*rn) < 0) (*rn)--;
+                  else (*rn)++;
+				   }
+					mpn_sub_1(sp + s2, r1 + s2, s1 - s2, cy&1);
+				} else // both inputs have same number of limbs
+				{
+               *rn = r1n;
+               if (cy>>1) 
+               {
+                  rp[s1] = 1;
+                  if ((*rn) < 0) (*rn)--;
+                  else (*rn)++;
+				   }
+				}
+				*sn = s1;
+            MPN_NORMALIZE(sp, (*sn));
+			   if (r1n ^ (~swapped) < 0) *sn = -(*sn);
+			} else // inputs are different sign
+			{
+				cy = mpn_sumdiff_n(sp, rp, r1, r2, s2);
+				if (s1 > s2) // r1 has more limbs than r2
+				{
+               *sn = r1n;
+               cy2 = mpn_add_1(sp + s2, r1 + s2, s1 - s2, cy>>1);
+					if (cy2) 
+               {
+                  sp[s1] = cy2;
+                  if ((*sn) < 0) (*sn)--;
+                  else (*sn)++;
+				   }
+					mpn_sub_1(rp + s2, r1 + s2, s1 - s2, cy&1);
+				} else // both inputs have same number of limbs
+				{
+               *sn = r1n;
+               if (cy>>1) 
+               {
+                  sp[s1] = 1;
+                  if ((*sn) < 0) (*sn)--;
+                  else (*sn)++;
+				   }
+				}
+				*rn = s1;
+            MPN_NORMALIZE(rp, (*rn));
+			   if (r1n ^ (~swapped) < 0) *rn = -(*rn);
+			}
+      } else // r2 is bigger than r1 (but same number of limbs)
+      {
+         if ((r1n ^ r2n) >= 0) // both inputs are same sign
+			{
+				cy = mpn_sumdiff_n(rp, sp, r2, r1, s1);
+				*rn = r1n;
+            if (cy>>1) 
+            {
+               rp[s1] = 1;
+               if ((*rn) < 0) (*rn)--;
+               else (*rn)++;
+				}
+				*sn = s1;
+            MPN_NORMALIZE(sp, (*sn));
+			   if (r1n ^ (~swapped) > 0) *sn = -(*sn);
+			} else // inputs are different sign
+			{
+				cy = mpn_sumdiff_n(sp, rp, r2, r1, s1);
+				*sn = r1n;
+            if (cy>>1) 
+            {
+               sp[s1] = 1;
+               if ((*sn) < 0) (*sn)--;
+               else (*sn)++;
+				}
+				*rn = s1;
+            MPN_NORMALIZE(rp, (*rn));
+			   if (r1n ^ (~swapped) > 0) *rn = -(*rn);
+			}
+      }
+   }
+}
+
+void tc4_sumdiff_unsigned(mp_ptr rp, mp_size_t * rn, mp_ptr sp, mp_size_t * sn, mp_ptr r1, mp_size_t r1n, mp_ptr r2, mp_size_t r2n)
+{
+   mp_limb_t cy, cy2;
+   mp_size_t s1 = ABS(r1n);
+   mp_size_t s2 = ABS(r2n);
+	int swapped = 0;
+   
+   if (s1 < s2) 
+   {
+      MPN_PTR_SWAP(r1, r1n, r2, r2n);
+      MP_SIZE_T_SWAP(s1, s2);
+		swapped = 1;
+   } 
+   
+   if (!s1)
+   {
+      *rn = 0;
+		*sn = 0;
+   } else if (!s2)
+   {
+      if (rp != r1) MPN_COPY(rp, r1, s1);
+		if (sp != r1) MPN_COPY(sp, r1, s1);
+		*rn = r1n;
+		*sn = (swapped ? -r1n : r1n);
+   } else
+   {
+      mp_size_t ct;
+		if (s1 != s2) ct = 1;
+		else MPN_CMP(ct, r1, r2, s1); 
+		    
+      if (!ct) 
+		{
+			*sn = 0;
+		   *rn = r1n;
+         cy = mpn_lshift(rp, r1, s1, 1);
+         if (cy) 
+         {
+            rp[s1] = cy;
+            (*rn)++;
+		   }
+		} else if (ct > 0) // r1 is bigger than r2
+      {
+         cy = mpn_sumdiff_n(rp, sp, r1, r2, s2);
+			if (s1 > s2) // r1 has more limbs than r2
+			{
+            *rn = r1n;
+            cy2 = mpn_add_1(rp + s2, r1 + s2, s1 - s2, cy>>1);
+				if (cy2) 
+            {
+               rp[s1] = cy2;
+               (*rn)++;
+				}
+			   mpn_sub_1(sp + s2, r1 + s2, s1 - s2, cy&1);
+			} else // both inputs have same number of limbs
+			{
+            *rn = r1n;
+            if (cy>>1) 
+            {
+               rp[s1] = 1;
+               (*rn)++;
+				}
+		   }
+			*sn = s1;
+         MPN_NORMALIZE(sp, (*sn));
+			if (swapped) *sn = -(*sn);
+      } else // r2 is bigger than r1 (but same number of limbs)
+      {
+         cy = mpn_sumdiff_n(rp, sp, r2, r1, s1);
+			*rn = r1n;
+         if (cy>>1) 
+         {
+            rp[s1] = 1;
+            (*rn)++;
+			}
+			*sn = s1;
+         MPN_NORMALIZE(sp, (*sn));
+			if (!swapped) *sn = -(*sn);
+      }
+   }
+}
+
+void tc4_add_unsigned(mp_ptr rp, mp_size_t * rn, mp_ptr r1, mp_size_t r1n, mp_ptr r2, mp_size_t r2n)
+{
+   mp_limb_t cy;
+   mp_size_t s1 = ABS(r1n);
+   mp_size_t s2 = ABS(r2n);
+   
+   if (s1 < s2) 
+   {
+      MPN_PTR_SWAP(r1, r1n, r2, r2n);
+      MP_SIZE_T_SWAP(s1, s2);
+   } 
+   
+   if (!s2)
+   {
+      if (!s1) *rn = 0;
+      else
+      {
+         if (rp != r1) MPN_COPY(rp, r1, s1);
+		   *rn = r1n;
+		}
+   } else
+   {
+      *rn = r1n;
+      cy = mpn_add(rp, r1, s1, r2, s2);
+      if (cy) 
+      {
+         rp[s1] = cy;
+         if ((*rn) < 0) (*rn)--;
+         else (*rn)++;
+      }
+   } 
+}
+
 inline 
 void tc4_sub(mp_ptr rp, mp_size_t * rn, mp_ptr r1, mp_size_t r1n, mp_ptr r2, mp_size_t r2n)
 {
@@ -195,14 +454,14 @@ void tc4_divexact_by3(mp_ptr rp, mp_size_t * rn, mp_ptr x, mp_size_t xn)
 	if (xn)
 	{
 		mp_size_t xu = ABS(xn);
-		mp_limb_t cy = mpn_divexact_by3(rp, x, xu);
+		mpn_divexact_by3(rp, x, xu);
 		if (xn > 0)
 		{
-			if (rp[xu - 1] == 0) *rn = xn - 1;
+			if (rp[xu - 1] == CNST_LIMB(0)) *rn = xn - 1;
 			else *rn = xn;
 		} else
 		{
-			if (rp[xu - 1] == 0) *rn = xn + 1;
+			if (rp[xu - 1] == CNST_LIMB(0)) *rn = xn + 1;
 			else *rn = xn;
 		}	
 	} else *rn = 0;
@@ -527,15 +786,17 @@ mpn_mul_tc4 (mp_ptr rp, mp_ptr up,
 #define r7 (tp + 6*t4)
 #define temp (tp + 7*t4)
 
-   tc4_add(r6, &n6, a3, a3n, a1, a1n); 
-   tc4_add(r5, &n5, a2, a2n, a0, a0n); 
-	tc4_add(r3, &n3, r5, n5, r6, n6); 
-   tc4_sub(r4, &n4, r5, n5, r6, n6);
-   tc4_add(r6, &n6, b3, b3n, b1, b1n);
-   tc4_add(r5, &n5, b2, b2n, b0, b0n);
-   tc4_add(r7, &n7, r5, n5, r6, n6); 
-   tc4_sub(r5, &n5, r5, n5, r6, n6);
-   
+   tc4_add_unsigned(r6, &n6, a3, a3n, a1, a1n); 
+   tc4_add_unsigned(r5, &n5, a2, a2n, a0, a0n); 
+	//tc4_add_unsigned(r3, &n3, r5, n5, r6, n6); //##############
+   //tc4_sub(r4, &n4, r5, n5, r6, n6);
+   tc4_sumdiff_unsigned(r3, &n3, r4, &n4, r5, n5, r6, n6); 
+	tc4_add_unsigned(r6, &n6, b3, b3n, b1, b1n);
+   tc4_add_unsigned(r5, &n5, b2, b2n, b0, b0n);
+   //tc4_add_unsigned(r7, &n7, r5, n5, r6, n6); //################
+   //tc4_sub(r5, &n5, r5, n5, r6, n6);
+   tc4_sumdiff_unsigned(r7, &n7, r5, &n5, r5, n5, r6, n6); 
+	
 	MUL_TC4_UNSIGNED(r3, n3, r3, n3, r7, n7, temp);
 	MUL_TC4(r4, n4, r4, n4, r5, n5, temp);
    
@@ -577,28 +838,28 @@ mpn_mul_tc4 (mp_ptr rp, mp_ptr up,
    tc4_rshift_inplace(r4, &n4, 1);
 	
 	tc4_sub(r3, &n3, r3, n3, r4, n4);
-	tc4_lshift(r5, &n5, r5, n5, 1);
+	tc4_lshift(r5, &n5, r5, n5, 1); //###############
 	tc4_sub(r5, &n5, r5, n5, r6, n6);
    
    tc4_submul_1(r2, &n2, r3, n3, 65);
-	tc4_sub(r3, &n3, r3, n3, r7, n7);
+	tc4_sub(r3, &n3, r3, n3, r7, n7); //################
    tc4_sub(r3, &n3, r3, n3, r1, n1);
    
    tc4_addmul_1(r2, &n2, r3, n3, 45);
    tc4_submul_1(r5, &n5, r3, n3, 8);
    
 	//tc4_divexact_ui(r5, &n5, r5, n5, 24);
-   tc4_divexact_by3(r5, &n5, r5, n5);
-   tc4_rshift_inplace(r5, &n5, 2);
-	
+   tc4_rshift_inplace(r5, &n5, 3);
+	tc4_divexact_by3(r5, &n5, r5, n5);
+   
 	tc4_sub(r6, &n6, r6, n6, r2, n2);
 	tc4_submul_1(r2, &n2, r4, n4, 16);
    
    //tc4_divexact_ui(r2, &n2, r2, n2, 18);
-   tc4_divexact_by3(r2, &n2, r2, n2);
-   tc4_divexact_by3(r2, &n2, r2, n2);
    tc4_rshift_inplace(r2, &n2, 1);
-	
+	tc4_divexact_by3(r2, &n2, r2, n2);
+   tc4_divexact_by3(r2, &n2, r2, n2);
+   
    tc4_sub(r3, &n3, r3, n3, r5, n5);
 	tc4_sub(r4, &n4, r4, n4, r2, n2);
 	
@@ -677,7 +938,6 @@ mp_size_t randsize(mp_size_t limit)
 
 	free(up);
 	free(vp);
-   free(rp);
 
 	return 0;
 }*/
