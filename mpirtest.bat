706510c6cbf4f4a73cb99d187fdb840a95a62020
jasonmoxham <jason@njkfrudils.plus.com>
2009-08-19 12:46:48 +0000

    update automatic windows tests

--- mpirtest.bat
+++ mpirtest.bat
@@ -5,22 +5,22 @@ if "%1" == "" (
 )
 pushd %1\build.vc9
 call configure && call make && call make check && call make clean
-if %errorlevel% 1 (
+if errorlevel 1 (
 	echo "ERROR"
 	exit /b 1
 )
 call configure --enable-shared && call make && call make clean
-if %errorlevel% 1 (
+if errorlevel 1 (
 	echo "ERROR --enable-shared"
 	exit /b 1
 )
 call configure --cpu-none && call make && call make check && call make clean
-if %errorlevel% 1 (
+if errorlevel 1 (
 	echo "ERROR --cpu-none"
 	exit /b 1
 )
 call configure --cpu-none --enable-shared && call make && call make clean
-if %errorlevel% 1 (
+if errorlevel 1 (
 	echo "ERROR --cpu-none --enable-shared"
 	exit /b 1
 )
